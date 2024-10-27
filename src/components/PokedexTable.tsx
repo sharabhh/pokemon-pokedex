f2201c0b54fd53cb243f0792f33f9fea191ec796
sharabh mishra <sharabhmishra15@gmail.com>
2024-10-27 10:34:38 +0530

    typescript optimised

--- src/components/PokedexTable.tsx
+++ src/components/PokedexTable.tsx
@@ -1,7 +1,17 @@
 import { PokemonRow } from './PokemonRow';
 
-// @ts-expect-error
-export const PokedexTable = ({ pokemonArray }) => (
+interface Pokemon {
+  id: number;
+  name: string;
+  types: string[];
+  sprite: string;
+}
+
+interface PokedexTableProps {
+  pokemonArray: Pokemon[];
+}
+
+export const PokedexTable: React.FC<PokedexTableProps> = ({ pokemonArray }) => (
   <div className="pokedex-table">
     {pokemonArray.map((pokemon: any) => (
       <PokemonRow key={pokemon.id} pokemon={pokemon} />
--- src/components/PokemonTypeSelection.tsx
+++ src/components/PokemonTypeSelection.tsx
@@ -1,21 +1,27 @@
-// @ts-expect-error
-export const PokemonTypeSelection = ({ selectedType, selectType }) => {
-    const types = ['grass', 'fire', 'water', 'electric', 'psychic', 'rock'];
-  
-    return (
-      <div>
-        <select
-          value={selectedType || ''}
-          onChange={(e) => selectType(e.target.value || undefined)}
-        >
-          <option value="">All Types</option>
-          {types.map((type) => (
-            <option key={type} value={type}>
-              {type}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
-  };
-  
+interface PokemonTypeSelectionProps {
+  selectedType: string | undefined;
+  selectType: (type: string | undefined) => void;
+}
+
+export const PokemonTypeSelection: React.FC<PokemonTypeSelectionProps> = ({
+  selectedType,
+  selectType,
+}) => {
+  const types = ["grass", "fire", "water", "electric", "psychic", "rock"];
+
+  return (
+    <div>
+      <select
+        value={selectedType || ""}
+        onChange={(e) => selectType(e.target.value || undefined)}
+      >
+        <option value="">All Types</option>
+        {types.map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
